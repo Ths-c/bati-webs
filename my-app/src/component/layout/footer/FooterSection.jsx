92030f8948c3de934c31d3b7878b1a879998144d
Juan Ipata <ipatajuanignacio@gmail.com>
2026-06-21 21:44:16 -0300

    feat: add footer

--- my-app/src/component/layout/footer/FooterSection.jsx
+++ my-app/src/component/layout/footer/FooterSection.jsx
@@ -1,10 +1,20 @@
-
+import Bat from '../../layout/preloader/Bat';
 
 export default function FooterSection() {
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Bati Web. Todos los derechos reservados.</p>
+        <div className="flex items-center">
+          <Bat flying={false} />
+          <div className="flex flex-col items-start ml-4">
+            <h3 className="text-2xl font-bold">Bati Web</h3>
+            <p className="text-sm mb-4">Convertimos tus ideas en realidad.</p>
+          </div>
+        </div>
+
+        <p className="text-left ml-2">
+          &copy; {new Date().getFullYear()} Bati Web. Todos los derechos reservados.
+        </p>
       </div>
     </footer>
   );
